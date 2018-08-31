dashpay/dash-dev-branches

Classes, validation and update logic for CProUpRevTX

src/evo/providertx.cpp
@@ -242,6 +242,30 @@ bool CheckProUpRegTx(const CTransaction& tx, const CBlockIndex* pindex, CValidat
     return true;
 }
 
+bool CheckProUpRevTx(const CTransaction& tx, const CBlockIndex* pindex, CValidationState& state)
+{
+    AssertLockHeld(cs_main);
+
+    CProUpRevTX ptx;
+    if (!GetTxPayload(tx, ptx))
+        return state.DoS(100, false, REJECT_INVALID, "bad-tx-payload");
+
+    if (ptx.nVersion != CProRegTX::CURRENT_VERSION)
+        return state.DoS(100, false, REJECT_INVALID, "bad-protx-version");
+
+    if (pindex) {
+        auto mnList = deterministicMNManager->GetListAtHeight(pindex->nHeight - 1);
+        auto dmn = mnList.GetMN(ptx.proTxHash);
+        if (!dmn)
+            return state.DoS(100, false, REJECT_INVALID, "bad-protx-hash");
+
+        if (!CheckInputsHashAndSig(tx, ptx, dmn->state->keyIDOperator, state))
+            return false;
+    }
+
+    return true;
+}
+
 std::string CProRegTX::ToString() const
 {
     CTxDestination dest;
@@ -329,6 +353,21 @@ void CProUpRegTX::ToJson(UniValue& obj) const
         CBitcoinAddress bitcoinAddress(dest);
         obj.push_back(Pair("payoutAddress", bitcoinAddress.ToString()));
     }
+    obj.push_back(Pair("inputsHash", inputsHash.ToString()));
+}
+
+std::string CProUpRevTX::ToString() const
+{
+    return strprintf("CProUpRevTX(nVersion=%d, proTxHash=%s)",
+                     nVersion, proTxHash.ToString());
+}
+
+void CProUpRevTX::ToJson(UniValue& obj) const
+{
+    obj.clear();
+    obj.setObject();
+    obj.push_back(Pair("version", nVersion));
+    obj.push_back(Pair("proTxHash", proTxHash.ToString()));
     obj.push_back(Pair("inputsHash", inputsHash.ToString()));
 }
 

src/evo/providertx.h
@@ -129,10 +129,41 @@ public:
     void ToJson(UniValue& obj) const;
 };
 
+class CProUpRevTX
+{
+public:
+    static const int CURRENT_VERSION = 1;
+
+public:
+    uint16_t nVersion{CURRENT_VERSION}; // message version
+    uint256 proTxHash;
+    uint256 inputsHash; // replay protection
+    std::vector<unsigned char> vchSig;
+
+public:
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action)
+    {
+        READWRITE(nVersion);
+        READWRITE(proTxHash);
+        READWRITE(inputsHash);
+        if (!(s.GetType() & SER_GETHASH)) {
+            READWRITE(vchSig);
+        }
+    }
+
+public:
+    std::string ToString() const;
+    void ToJson(UniValue& obj) const;
+};
+
 
 bool CheckProRegTx(const CTransaction& tx, const CBlockIndex* pindex, CValidationState& state);
 bool CheckProUpServTx(const CTransaction& tx, const CBlockIndex* pindex, CValidationState& state);
 bool CheckProUpRegTx(const CTransaction& tx, const CBlockIndex* pindex, CValidationState& state);
+bool CheckProUpRevTx(const CTransaction& tx, const CBlockIndex* pindex, CValidationState& state);
 
 bool IsProTxCollateral(const CTransaction& tx, uint32_t n);
 uint32_t GetProTxCollateralIndex(const CTransaction& tx);

src/evo/specialtx.cpp
@@ -33,6 +33,8 @@ bool CheckSpecialTx(const CTransaction& tx, const CBlockIndex* pindex, CValidati
             return CheckProUpServTx(tx, pindex, state);
         case TRANSACTION_PROVIDER_UPDATE_REGISTRAR:
             return CheckProUpRegTx(tx, pindex, state);
+        case TRANSACTION_PROVIDER_UPDATE_REVOKE:
+            return CheckProUpRevTx(tx, pindex, state);
     }
 
     return state.DoS(100, false, REJECT_INVALID, "bad-tx-type");
@@ -47,6 +49,7 @@ bool ProcessSpecialTx(const CTransaction& tx, const CBlockIndex* pindex, CValida
         case TRANSACTION_PROVIDER_REGISTER:
         case TRANSACTION_PROVIDER_UPDATE_SERVICE:
         case TRANSACTION_PROVIDER_UPDATE_REGISTRAR:
+        case TRANSACTION_PROVIDER_UPDATE_REVOKE:
             return true; // handled in batches per block
     }
 
@@ -62,6 +65,7 @@ bool UndoSpecialTx(const CTransaction& tx, const CBlockIndex* pindex)
         case TRANSACTION_PROVIDER_REGISTER:
         case TRANSACTION_PROVIDER_UPDATE_SERVICE:
         case TRANSACTION_PROVIDER_UPDATE_REGISTRAR:
+        case TRANSACTION_PROVIDER_UPDATE_REVOKE:
             return true; // handled in batches per block
     }
 

bytzck/BYTZ

src/evo/providertx.cpp
@@ -217,6 +217,33 @@ bool CheckProUpRegTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CVal
     return true;
 }
 
+bool CheckProUpRevTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CValidationState& state)
+{
+    AssertLockHeld(cs_main);
+
+    CProUpRevTx ptx;
+    if (!GetTxPayload(tx, ptx))
+        return state.DoS(100, false, REJECT_INVALID, "bad-tx-payload");
+
+    if (ptx.nVersion > CProRegTx::CURRENT_VERSION)
+        return state.DoS(100, false, REJECT_INVALID, "bad-protx-version");
+
+    if (ptx.nReason < CProUpRevTx::REASON_NOT_SPECIFIED || ptx.nReason > CProUpRevTx::REASON_LAST)
+        return state.DoS(100, false, REJECT_INVALID, "bad-protx-reason");
+
+    if (pindexPrev) {
+        auto mnList = deterministicMNManager->GetListForBlock(pindexPrev->GetBlockHash());
+        auto dmn = mnList.GetMN(ptx.proTxHash);
+        if (!dmn)
+            return state.DoS(100, false, REJECT_INVALID, "bad-protx-hash");
+
+        if (!CheckInputsHashAndSig(tx, ptx, dmn->pdmnState->keyIDOperator, state))
+            return false;
+    }
+
+    return true;
+}
+
 std::string CProRegTx::ToString() const
 {
     CTxDestination dest;
@@ -304,6 +331,22 @@ void CProUpRegTx::ToJson(UniValue& obj) const
         CBitcoinAddress bitcoinAddress(dest);
         obj.push_back(Pair("payoutAddress", bitcoinAddress.ToString()));
     }
+    obj.push_back(Pair("inputsHash", inputsHash.ToString()));
+}
+
+std::string CProUpRevTx::ToString() const
+{
+    return strprintf("CProUpRevTx(nVersion=%d, proTxHash=%s, nReason=%d)",
+                     nVersion, proTxHash.ToString(), nReason);
+}
+
+void CProUpRevTx::ToJson(UniValue& obj) const
+{
+    obj.clear();
+    obj.setObject();
+    obj.push_back(Pair("version", nVersion));
+    obj.push_back(Pair("proTxHash", proTxHash.ToString()));
+    obj.push_back(Pair("reason", (int)nReason));
     obj.push_back(Pair("inputsHash", inputsHash.ToString()));
 }
 

src/evo/providertx.h
@@ -129,10 +129,52 @@ public:
     void ToJson(UniValue& obj) const;
 };
 
+class CProUpRevTx
+{
+public:
+    static const uint16_t CURRENT_VERSION = 1;
+
+    // these are just informational and do not have any effect on the revocation
+    enum {
+        REASON_NOT_SPECIFIED = 0,
+        REASON_TERMINATION_OF_SERVICE = 1,
+        REASON_COMPROMISED_KEYS = 2,
+        REASON_CHANGE_OF_KEYS = 3,
+        REASON_LAST = REASON_CHANGE_OF_KEYS
+    };
+
+public:
+    uint16_t nVersion{CURRENT_VERSION}; // message version
+    uint256 proTxHash;
+    uint16_t nReason{REASON_NOT_SPECIFIED};
+    uint256 inputsHash; // replay protection
+    std::vector<unsigned char> vchSig;
+
+public:
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action)
+    {
+        READWRITE(nVersion);
+        READWRITE(proTxHash);
+        READWRITE(nReason);
+        READWRITE(inputsHash);
+        if (!(s.GetType() & SER_GETHASH)) {
+            READWRITE(vchSig);
+        }
+    }
+
+public:
+    std::string ToString() const;
+    void ToJson(UniValue& obj) const;
+};
+
 
 bool CheckProRegTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CValidationState& state);
 bool CheckProUpServTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CValidationState& state);
 bool CheckProUpRegTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CValidationState& state);
+bool CheckProUpRevTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CValidationState& state);
 
 bool IsProTxCollateral(const CTransaction& tx, uint32_t n);
 uint32_t GetProTxCollateralIndex(const CTransaction& tx);

src/evo/specialtx.cpp
@@ -31,6 +31,8 @@ bool CheckSpecialTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CVali
             return CheckProUpServTx(tx, pindexPrev, state);
         case TRANSACTION_PROVIDER_UPDATE_REGISTRAR:
             return CheckProUpRegTx(tx, pindexPrev, state);
+        case TRANSACTION_PROVIDER_UPDATE_REVOKE:
+            return CheckProUpRevTx(tx, pindexPrev, state);
     }
 
     return state.DoS(10, false, REJECT_INVALID, "bad-tx-type");
@@ -45,6 +47,7 @@ bool ProcessSpecialTx(const CTransaction& tx, const CBlockIndex* pindex, CValida
         case TRANSACTION_PROVIDER_REGISTER:
         case TRANSACTION_PROVIDER_UPDATE_SERVICE:
         case TRANSACTION_PROVIDER_UPDATE_REGISTRAR:
+        case TRANSACTION_PROVIDER_UPDATE_REVOKE:
             return true; // handled in batches per block
     }
 
@@ -60,6 +63,7 @@ bool UndoSpecialTx(const CTransaction& tx, const CBlockIndex* pindex)
         case TRANSACTION_PROVIDER_REGISTER:
         case TRANSACTION_PROVIDER_UPDATE_SERVICE:
         case TRANSACTION_PROVIDER_UPDATE_REGISTRAR:
+        case TRANSACTION_PROVIDER_UPDATE_REVOKE:
             return true; // handled in batches per block
     }
 