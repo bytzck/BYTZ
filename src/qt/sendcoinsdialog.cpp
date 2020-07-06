xdustinface/dash

qt: Move font-size settings from css to c++.
This also keeps track of changed font-sizes and updates them depending on the settings if GUIUtil::updateFonts() gets called.

src/qt/sendcoinsdialog.cpp
@@ -79,6 +79,13 @@ SendCoinsDialog::SendCoinsDialog(const PlatformStyle *_platformStyle, QWidget *p
                       ui->labelBalanceText
                      }, GUIUtil::getFontWeightBold());
 
+    GUIUtil::setFont({ui->labelBalance,
+                      ui->labelBalanceText
+                     }, GUIUtil::Weight::Bold, 14);
+
+    GUIUtil::setFont({ui->labelCoinControlFeatures
+                     }, GUIUtil::getFontWeightBold(), 16);
+
     if (!_platformStyle->getImagesOnButtons()) {
         ui->addButton->setIcon(QIcon());
         ui->clearButton->setIcon(QIcon());

bytzck/BYTZ

src/qt/sendcoinsdialog.cpp
@@ -74,10 +74,15 @@ SendCoinsDialog::SendCoinsDialog(const PlatformStyle *_platformStyle, QWidget *p
                       ui->labelCoinControlAfterFeeText,
                       ui->labelCoinControlChangeText,
                       ui->labelFeeHeadline,
-                      ui->fallbackFeeWarningLabel,
-                      ui->labelBalance,
-                      ui->label
+                      ui->fallbackFeeWarningLabel
                      }, GUIUtil::getFontWeightBold());
+
+    GUIUtil::setFont({ui->labelBalance,
+                      ui->label
+                     }, GUIUtil::getFontWeightBold(), 14);
+
+    GUIUtil::setFont({ui->labelCoinControlFeatures
+                     }, GUIUtil::getFontWeightBold(), 16);
 
     if (!_platformStyle->getImagesOnButtons()) {
         ui->addButton->setIcon(QIcon());