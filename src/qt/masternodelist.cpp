xdustinface/dash

interface/qt: Remove direct src/masterode calls
Refactored into interface::Node::Masternode

src/qt/masternodelist.cpp
@@ -1,15 +1,12 @@
 #include <qt/masternodelist.h>
 #include <qt/forms/ui_masternodelist.h>
 
-#include <masternode/activemasternode.h>
 #include <qt/clientmodel.h>
 #include <clientversion.h>
 #include <coins.h>
 #include <qt/guiutil.h>
 #include <init.h>
-#include <masternode/masternode-sync.h>
 #include <netbase.h>
-#include <validation.h>
 #include <qt/walletmodel.h>
 
 #include <univalue.h>
@@ -144,7 +141,7 @@ void MasternodeList::updateDIP3ListScheduled()
             fFilterUpdatedDIP3 = false;
         }
     } else if (mnListChanged) {
-        int64_t nMnListUpdateSecods = masternodeSync.IsBlockchainSynced() ? MASTERNODELIST_UPDATE_SECONDS : MASTERNODELIST_UPDATE_SECONDS*10;
+        int64_t nMnListUpdateSecods = clientModel->masternode().isBlockchainSynced() ? MASTERNODELIST_UPDATE_SECONDS : MASTERNODELIST_UPDATE_SECONDS*10;
         int64_t nSecondsToWait = nTimeUpdatedDIP3 - GetTime() + nMnListUpdateSecods;
 
         if (nSecondsToWait <= 0) {
@@ -166,11 +163,10 @@ void MasternodeList::updateDIP3List()
     {
         // Get all UTXOs for each MN collateral in one go so that we can reduce locking overhead for cs_main
         // We also do this outside of the below Qt list update loop to reduce cs_main locking time to a minimum
-        LOCK(cs_main);
         mnList.ForEachMN(false, [&](const CDeterministicMNCPtr& dmn) {
             CTxDestination collateralDest;
             Coin coin;
-            if (GetUTXOCoin(dmn->collateralOutpoint, coin) && ExtractDestination(coin.out.scriptPubKey, collateralDest)) {
+            if (clientModel->node().getUnspentOutput(dmn->collateralOutpoint, coin) && ExtractDestination(coin.out.scriptPubKey, collateralDest)) {
                 mapCollateralDests.emplace(dmn->proTxHash, collateralDest);
             }
         });

src/qt/overviewpage.cpp
@@ -17,7 +17,6 @@
 #include <qt/utilitydialog.h>
 #include <qt/walletmodel.h>
 
-#include <masternode/masternode-sync.h>
 #include <privatesend/privatesend-client.h>
 
 #include <QAbstractItemDelegate>
@@ -346,7 +345,7 @@ void OverviewPage::showOutOfSyncWarning(bool fShow)
 
 void OverviewPage::updatePrivateSendProgress()
 {
-    if(!masternodeSync.IsBlockchainSynced() || ShutdownRequested()) return;
+    if(!clientModel->masternode().isBlockchainSynced() || ShutdownRequested()) return;
 
     if(!walletModel) return;
 
@@ -468,7 +467,7 @@ void OverviewPage::updateAdvancedPSUI(bool fShowAdvancedPSUI) {
 
 void OverviewPage::privateSendStatus(bool fForce)
 {
-    if (!fForce && (!masternodeSync.IsBlockchainSynced() || ShutdownRequested())) return;
+    if (!fForce && (!clientModel->masternode().isBlockchainSynced() || ShutdownRequested())) return;
 
     if(!walletModel) return;
 

bytzck/BYTZ

src/qt/masternodelist.cpp
@@ -1,15 +1,12 @@
 #include <qt/masternodelist.h>
 #include <qt/forms/ui_masternodelist.h>
 
-#include <masternode/activemasternode.h>
 #include <qt/clientmodel.h>
 #include <clientversion.h>
 #include <coins.h>
 #include <qt/guiutil.h>
 #include <init.h>
-#include <masternode/masternode-sync.h>
 #include <netbase.h>
-#include <validation.h>
 #include <qt/walletmodel.h>
 
 #include <univalue.h>
@@ -146,7 +143,7 @@ void MasternodeList::updateDIP3ListScheduled()
             fFilterUpdatedDIP3 = false;
         }
     } else if (mnListChanged) {
-        int64_t nMnListUpdateSecods = masternodeSync.IsBlockchainSynced() ? MASTERNODELIST_UPDATE_SECONDS : MASTERNODELIST_UPDATE_SECONDS*10;
+        int64_t nMnListUpdateSecods = clientModel->masternodeSync().isBlockchainSynced() ? MASTERNODELIST_UPDATE_SECONDS : MASTERNODELIST_UPDATE_SECONDS * 10;
         int64_t nSecondsToWait = nTimeUpdatedDIP3 - GetTime() + nMnListUpdateSecods;
 
         if (nSecondsToWait <= 0) {
@@ -168,11 +165,10 @@ void MasternodeList::updateDIP3List()
     {
         // Get all UTXOs for each MN collateral in one go so that we can reduce locking overhead for cs_main
         // We also do this outside of the below Qt list update loop to reduce cs_main locking time to a minimum
-        LOCK(cs_main);
         mnList.ForEachMN(false, [&](const CDeterministicMNCPtr& dmn) {
             CTxDestination collateralDest;
             Coin coin;
-            if (GetUTXOCoin(dmn->collateralOutpoint, coin) && ExtractDestination(coin.out.scriptPubKey, collateralDest)) {
+            if (clientModel->node().getUnspentOutput(dmn->collateralOutpoint, coin) && ExtractDestination(coin.out.scriptPubKey, collateralDest)) {
                 mapCollateralDests.emplace(dmn->proTxHash, collateralDest);
             }
         });

src/qt/overviewpage.cpp
@@ -16,7 +16,6 @@
 #include <qt/utilitydialog.h>
 #include <qt/walletmodel.h>
 
-#include <masternode/masternode-sync.h>
 #include <privatesend/privatesend-client.h>
 
 #include <QAbstractItemDelegate>
@@ -321,7 +320,7 @@ void OverviewPage::showOutOfSyncWarning(bool fShow)
 
 void OverviewPage::updatePrivateSendProgress()
 {
-    if(!masternodeSync.IsBlockchainSynced() || ShutdownRequested()) return;
+    if(!clientModel->masternodeSync().isBlockchainSynced() || ShutdownRequested()) return;
 
     if(!walletModel) return;
 
@@ -434,7 +433,7 @@ void OverviewPage::updateAdvancedPSUI(bool fShowAdvancedPSUI) {
 
 void OverviewPage::privateSendStatus(bool fForce)
 {
-    if (!fForce && (!masternodeSync.IsBlockchainSynced() || ShutdownRequested())) return;
+    if (!fForce && (!clientModel->masternode().isBlockchainSynced() || ShutdownRequested())) return;
 
     if(!walletModel) return;
 