ioncoincore/ion-bitbucket

Enable verification of token database
When verification fails, the user either needs to start
with -reindex-tokens manually, or let the wallet reindex itself.
Around the upgrade to 5.0, a full reindex is a sane default.

- Rename -reindextokens to -reindex-tokens
- Move ReindexTokenDB after setting chainActive
- Add VerifyTokenDB
- Add block hash of token creation to db
- Do not buffer tokens when reindexing

src/tokens/tokengroupmanager.cpp
@@ -89,7 +89,7 @@ void CTokenGroupManager::ResetTokenGroups() {
     CTransaction tgTxION;
     CTokenGroupDescription tgDescriptionION("ION", "Ion", 8, "https://www.ionomy.com", uint256());
     CTokenGroupStatus tokenGroupStatus;
-    CTokenGroupCreation tgCreationION(MakeTransactionRef(tgTxION), tgInfoION, tgDescriptionION, tokenGroupStatus);
+    CTokenGroupCreation tgCreationION(MakeTransactionRef(tgTxION), uint256(), tgInfoION, tgDescriptionION, tokenGroupStatus);
     mapTokenGroups.insert(std::pair<CTokenGroupID, CTokenGroupCreation>(NoGroup, tgCreationION));
 
 }

bytzck/BYTZ

src/tokens/tokengroupmanager.cpp
@@ -89,7 +89,7 @@ void CTokenGroupManager::ResetTokenGroups() {
     CTransaction tgTxBytz;
     CTokenGroupDescription tgDescriptionBYTZ("BYTZ", "Bytz", 8, "https://bytz.gg", uint256());
     CTokenGroupStatus tokenGroupStatus;
-    CTokenGroupCreation tgCreationBYTZ(MakeTransactionRef(tgTxBytz), tgInfoBYTZ, tgDescriptionBYTZ, tokenGroupStatus);
+    CTokenGroupCreation tgCreationBYTZ(MakeTransactionRef(tgTxBytz), uint256(), tgInfoBYTZ, tgDescriptionBYTZ, tokenGroupStatus);
     mapTokenGroups.insert(std::pair<CTokenGroupID, CTokenGroupCreation>(NoGroup, tgCreationBYTZ));
 
 }