5c5b1f3593f4e99893dd9c138ac1b38982468ebd
bytzck <bytzck@i2pmail.org>
2022-01-08 12:02:24 +0000

    Revert to original depends

--- depends/packages/native_biplist.mk
+++ depends/packages/native_biplist.mk
@@ -1,10 +1,9 @@
-package=native_ds_store
-$(package)_version=1.1.2
-$(package)_download_path=https://github.com/al45tair/ds_store/archive/
-$(package)_file_name=v$($(package)_version).tar.gz
-$(package)_sha256_hash=3b3ecb7bf0a5157f5b6010bc3af7c141fb0ad3527084e63336220d22744bc20c
+package=native_biplist
+$(package)_version=1.0.3
+$(package)_download_path=https://bitbucket.org/wooster/biplist/downloads
+$(package)_file_name=biplist-$($(package)_version).tar.gz
+$(package)_sha256_hash=4c0549764c5fe50b28042ec21aa2e14fe1a2224e239a1dae77d9e7f3932aa4c6
 $(package)_install_libdir=$(build_prefix)/lib/python/dist-packages
-$(package)_dependencies=native_biplist
 
 define $(package)_build_cmds
     python3 setup.py build
