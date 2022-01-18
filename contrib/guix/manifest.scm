d7a6ac25f80e1df85aaa481fbc934695de0f21d2
bytzck <bytzck@i2pmail.org>
2022-01-18 13:36:39 +0000

    Remove clang 10

--- contrib/guix/manifest.scm
+++ contrib/guix/manifest.scm
@@ -656,5 +656,5 @@ inspecting signatures in Mach-O binaries.")
                        (else
                         (make-bitcoin-cross-toolchain target)))))
           ((string-contains target "darwin")
-           (list clang-toolchain-10 binutils imagemagick libtiff librsvg font-tuffy cmake xorriso python-signapple))
+           (list binutils imagemagick libtiff librsvg font-tuffy cmake xorriso python-signapple))
           (else '())))))
