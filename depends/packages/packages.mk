9105b865f75fce1f190546fd23a7bbcc80931e20
bytzck <bytzck@i2pmail.org>
2022-01-19 10:54:06 +0000

    REmove native_b2 2

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -20,8 +20,6 @@ multiprocess_native_packages = native_libmultiprocess native_capnp
 
 darwin_native_packages = native_ds_store native_mac_alias
 
-$(host_arch)_$(host_os)_native_packages
-
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_libtapi native_libdmg-hfsplus
 
