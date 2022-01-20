5a1973ebb7368f339775a895e85ad449799b855f
bytzck <bytzck@i2pmail.org>
2022-01-20 10:27:53 +0000

    Update new qt to work with bytz 6

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -31,6 +31,7 @@ $(package)_config_opts += -c++std c++11
 $(package)_config_opts += -confirm-license
 $(package)_config_opts += -dbus-runtime
 $(package)_config_opts += -feature-networkproxy
+$(package)_config_opts += -feature-secure-transport
 $(package)_config_opts += -feature-socks5
 $(package)_config_opts += -hostprefix $(build_prefix)
 $(package)_config_opts += -no-cups
