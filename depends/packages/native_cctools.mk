2fae47332024651b41bbd53331f473ac509b6a6c
bytzck <bytzck@i2pmail.org>
2022-01-25 12:03:54 +0000

    Ncurses removed 4

--- depends/packages/native_cctools.mk
+++ depends/packages/native_cctools.mk
@@ -10,7 +10,6 @@ $(package)_clang_download_file=clang+llvm-$($(package)_clang_version)-x86_64-lin
 $(package)_clang_file_name=clang-llvm-$($(package)_clang_version)-x86_64-linux-gnu-ubuntu-14.04.tar.xz
 $(package)_clang_sha256_hash=99b28a6b48e793705228a390471991386daa33a9717cd9ca007fcdde69608fd9
 $(package)_extra_sources=$($(package)_clang_file_name)
-$(package)_dependencies=ncurses
 
 define $(package)_fetch_cmds
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_download_file),$($(package)_file_name),$($(package)_sha256_hash)) && \
