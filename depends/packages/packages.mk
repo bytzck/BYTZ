8fdec3669e9a9597f52991a2b55f426b548bdb36
bytzck <bytzck@i2pmail.org>
2022-01-25 12:01:26 +0000

    Ncurses removed 2

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -12,11 +12,10 @@ wallet_packages=bdb
 
 upnp_packages=miniupnpc
 
-darwin_native_packages native_biplist native_ds_store native_mac_alias
+darwin_native_packages = native_biplist native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
-
 ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
 darwin_native_packages+= native_clang
 endif
