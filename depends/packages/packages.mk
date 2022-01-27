57b1c98b443d02d8e3170459f556b9c0dddaf67c
bytzck <bytzck@i2pmail.org>
2022-01-27 15:44:26 +0000

    New depends 2

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,6 +1,7 @@
-packages:=boost libevent
+packages:=boost libevent openssl zeromq gmp bls-dash backtrace cmake
 
 qrencode_packages = qrencode
+qt_native_packages = native_protobuf
 
 qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon
 qt_android_packages=qt
@@ -8,20 +9,13 @@ qt_android_packages=qt
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
 
-bdb_packages=bdb
-sqlite_packages=sqlite
+wallet_packages=bdb
 
 zmq_packages=zeromq
 
 upnp_packages=miniupnpc
-natpmp_packages=libnatpmp
-
-multiprocess_packages = libmultiprocess capnp
-multiprocess_native_packages = native_libmultiprocess native_capnp
 
 darwin_native_packages = native_ds_store native_mac_alias
-
-$(host_arch)_$(host_os)_native_packages += native_b2
 
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_libtapi native_libdmg-hfsplus
