68c472e3937ac6e5b51113bd1adc0d84ffab93a7
bytzck <bytzck@i2pmail.org>
2022-01-27 12:39:50 +0000

    Add in ncurses 8

--- contrib/guix/manifest.scm
+++ contrib/guix/manifest.scm
@@ -681,5 +681,5 @@ inspecting signatures in Mach-O binaries.")
                        (else
                         (make-bytz-cross-toolchain target)))))
           ((string-contains target "darwin")
-           (list clang-toolchain-10 ncurses binutils imagemagick libtiff librsvg font-tuffy cmake xorriso python-signapple))
+           (list clang-toolchain-8 binutils imagemagick libtiff librsvg font-tuffy cmake xorriso python-signapple))
           (else '())))))
