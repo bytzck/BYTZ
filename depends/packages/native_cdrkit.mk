9b1e64f950f2f67f0b652b5daac3da799e3df608
bytzck <bytzck@i2pmail.org>
2022-01-29 15:41:07 +0000

    Use built version of cmake 3

--- depends/packages/native_cdrkit.mk
+++ depends/packages/native_cdrkit.mk
@@ -11,7 +11,7 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_config_cmds
-  $(build_prefix)/../bin/cmake -DCMAKE_INSTALL_PREFIX=$(build_prefix)
+  $(build_prefix)/bin/cmake -DCMAKE_INSTALL_PREFIX=$(build_prefix)
 endef
 
 define $(package)_build_cmds
