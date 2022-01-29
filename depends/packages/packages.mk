be4567cf2873b84997d18c627bbcd228ffd15f51
bytzck <bytzck@i2pmail.org>
2022-01-29 11:07:59 +0000

    New depends 6

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,4 +1,4 @@
-packages:=boost libevent openssl zeromq gmp bls-dash backtrace cmake bdb
+packages:=boost openssl libevent zeromq gmp bls-dash backtrace cmake
 
 qrencode_packages = qrencode
 qt_native_packages = native_protobuf
