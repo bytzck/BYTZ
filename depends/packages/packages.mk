4ae809059db5e36216fdaca8f6eda416cd36896d
bytzck <bytzck@i2pmail.org>
2022-01-29 12:29:05 +0000

    New depends 9

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -15,7 +15,7 @@ zmq_packages=zeromq
 
 upnp_packages=miniupnpc
 
-darwin_native_packages = native_ds_store native_mac_alias native_backtrace
+darwin_native_packages = native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_libtapi native_libdmg-hfsplus native_clang
