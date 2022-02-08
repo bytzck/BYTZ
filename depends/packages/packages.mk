f73c0ec52cfaa3c57ad7c47fb703a4e014342910
bytzck <bytzck@i2pmail.org>
2022-02-08 12:12:41 +0000

    Revert mac builds to original mk 5

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -15,12 +15,12 @@ wallet_packages=bdb
 
 upnp_packages=miniupnpc
 
-darwin_native_packages = native_ds_store native_mac_alias
+darwin_native_packages = native_biplist native_ds_store native_mac_alias
 
 #$(host_arch)_$(host_os)_native_packages += native_b2
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_biplist native_ds_store native_mac_alias native_clang
+darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
 
 #ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
 #darwin_native_packages+= native_clang
