cb6a4b6d79eb3a9716c10686d26dd23c1a890cda
bytzck <bytzck@i2pmail.org>
2022-02-10 13:25:39 +0000

    Revert mac builds to original mk 63

--- depends/packages/native_clang.mk
+++ depends/packages/native_clang.mk
@@ -19,10 +19,11 @@ define $(package)_stage_cmds
   ls && \
   pwd && \
   ls $($(package)_extract_dir) && \
-  ls $($(package)_staging_prefix_dir) && \
-    mkdir -p $($(package)_staging_prefix_dir)/lib/clang/$($(package)_version)/include && \
+  mkdir -p $($(package)_staging_prefix_dir)/lib/clang/$($(package)_version)/include && \
   mkdir -p $($(package)_staging_prefix_dir)/bin && \
   mkdir -p $($(package)_staging_prefix_dir)/include && \
+  pwd && \
+  ls $($(package)_staging_prefix_dir) && \
   cp bin/clang $($(package)_staging_prefix_dir)/bin/ && \
   cp -P bin/clang++ $($(package)_staging_prefix_dir)/bin/ && \
   cp bin/dsymutil $($(package)_staging_prefix_dir)/bin/$(host)-dsymutil && \
