d8e91373b5e5c22692db8162e02288067b4e35b0
bytzck <bytzck@i2pmail.org>
2022-02-10 10:57:47 +0000

    Revert mac builds to original mk 46

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -23,8 +23,8 @@ ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_libtapi native_libdmg-hfsplus
 
 
-#ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
-#darwin_native_packages+= native_clang
-#endif
+ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
+darwin_native_packages+= native_clang
+endif
 
 endif
