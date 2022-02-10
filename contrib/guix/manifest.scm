c4124d2b34d36cab47c66a7e17f93ab543842046
bytzck <bytzck@i2pmail.org>
2022-02-10 14:20:09 +0000

    Revert mac builds to original mk 75

--- contrib/guix/manifest.scm
+++ contrib/guix/manifest.scm
@@ -282,13 +282,13 @@ parse, modify and abstract ELF, PE and MachO formats.")
                 "0pfzcl5g5ph9zywh5l5j1prhkwh2csnkmxl5sbkw9wcm17sskz48"))))
           (build-system gnu-build-system)
 
-          (arguments '(#:tests? #f)
-                     '(#:configure-flags `("--without-cxx"
+          (arguments '(#:tests? #f
+                      (#:configure-flags `("--without-cxx"
                        "--with-termlib"
                        "--enable-cxx-shared" 
                        "--enable-symlinks" 
                        "--with-shared" 
-                       "--without-tests")))
+                       "--without-tests"))))
 
     (home-page "https://invisible-island.net/ncurses/ncurses.html")
     (synopsis "Ncurses terminal programming")
