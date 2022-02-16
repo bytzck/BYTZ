2db4863136f014e6ebcbdeb772d4c66b2b21e7d5
bytzck <bytzck@i2pmail.org>
2022-02-16 15:15:06 +0000

    Back to original boost 3

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -25,7 +25,7 @@ endef
 
 define $(package)_preprocess_cmds
   echo "using $(boost_toolset_$(host_os)) : : $($(package)_cxx) : <cxxflags>\"$($(package)_cxxflags) $($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$(boost_archiver_$(host_os))\" <striper>\"$(host_STRIP)\"  <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam && /
-  ls ~/.guix-profile && /
+  ls ~/.guix-profile && \
   ls ~/.guix-profile/bin
 endef
 
