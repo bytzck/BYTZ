3902721b546071907099591ca20e228a3e2b9601
bytzck <bytzck@i2pmail.org>
2022-02-19 11:01:42 +0000

    Change ar and ranlib for darwin in zeroevent

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -6,6 +6,11 @@ $(package)_sha256_hash=bcbabe1e2c7d0eec4ed612e10b94b112dd5f06fcefa994a0c79a45d83
 $(package)_patches=remove_libstd_link.patch
 
 define $(package)_set_vars
+  ifneq (,$(findstring clang,$($(package)_cxx)))
+    $(package)_toolset_$(host_os)=clang
+    $(package)_archiver_$(host_os)=ar
+    $(package)_ranlib_$(host_os)=ranlib
+  endif
   $(package)_config_opts=--without-docs --disable-shared --disable-curve --disable-curve-keygen --disable-perf
   $(package)_config_opts += --without-libsodium --without-libgssapi_krb5 --without-pgm --without-norm --without-vmci
   $(package)_config_opts += --disable-libunwind --disable-radix-tree --without-gcov --disable-dependency-tracking
