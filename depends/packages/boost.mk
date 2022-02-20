18db86567521662afab1361ab7b4896495b46e0e
bytzck <bytzck@i2pmail.org>
2022-02-20 10:55:58 +0000

    Change to old version of boost.mk 3

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -16,17 +16,14 @@ $(package)_config_opts_x86_64_mingw32=address-model=64
 $(package)_config_opts_i686_mingw32=address-model=32
 $(package)_config_opts_i686_linux=address-model=32 architecture=x86
 ifneq (,$(findstring clang,$($(package)_cxx)))
-  $(package)_cc=clang
-  $(package)_cxx=clang++
+  $(package)_cc=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION)
+  $(package)_cxx=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++
   $(package)_ar=ar
   $(package)_ranlib=ranlib
   $(package)_libtool=libtool
 endif
-$(package)_config_opts_darwin += -device-option MAC_SDK_PATH=$(OSX_SDK)
-$(package)_config_opts_darwin += -device-option MAC_SDK_VERSION=$(OSX_SDK_VERSION)
 $(package)_toolset_$(host_os)=gcc
 $(package)_archiver_$(host_os)=$($(package)_ar)
-$(package)_toolset_darwin=clang-darwin
 $(package)_config_libraries=chrono,filesystem,program_options,system,thread,test
 $(package)_cxxflags=-std=c++11 -fvisibility=hidden
 $(package)_cxxflags_linux=-fPIC
