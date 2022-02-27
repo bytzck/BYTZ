720f6d73832ea836db8a9cc1e02306690cdd688e
bytzck <bytzck@i2pmail.org>
2022-02-27 10:39:45 +0000

    Change to newer libevent

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -1,8 +1,12 @@
 package=libevent
-$(package)_version=2.1.12-stable
-$(package)_download_path=https://github.com/libevent/libevent/releases/download/release-$($(package)_version)/
-$(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb
+$(package)_version=2.1.8
+$(package)_download_path=https://github.com/libevent/libevent/releases/download/release-$($(package)_version)-stable
+$(package)_file_name=$(package)-$($(package)_version)-stable.tar.gz
+$(package)_sha256_hash=965cc5a8bb46ce4199a47e9b2c9e1cae3b137e8356ffdad6d94d3b9069b71dc2
+
+define $(package)_preprocess_cmds
+  ./autogen.sh
+endef
 
 # When building for Windows, we set _WIN32_WINNT to target the same Windows
 # version as we do in configure. Due to quirks in libevents build system, this
