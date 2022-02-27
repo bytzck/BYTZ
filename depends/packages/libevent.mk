649057351a0e702eb5bfd7474d8086e9e49c2e04
bytzck <bytzck@i2pmail.org>
2022-02-27 10:42:24 +0000

    Change to newer libevent 2

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -16,6 +16,7 @@ define $(package)_set_vars
   $(package)_config_opts += --disable-dependency-tracking --enable-option-checking
   $(package)_config_opts_release=--disable-debug-mode
   $(package)_config_opts_linux=--with-pic
+  $(package)_config_opts_darwin="CC=clang CXX=clang++"
   $(package)_config_opts_android=--with-pic
   $(package)_cppflags_mingw32=-D_WIN32_WINNT=0x0601
 endef
