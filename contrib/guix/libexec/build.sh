5e8bbe81ad09eb8d4eedab84cb12f13e45d954b4
Bytzck <bytzck@protonmail.com>
2022-02-28 13:22:39 +0000

    REmove library removal as they don;t exist

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -357,12 +357,6 @@ mkdir -p "$DISTSRC"
     (
         cd installed
 
-        case "$HOST" in
-            *mingw*)
-                mv --target-directory="$DISTNAME"/lib/ "$DISTNAME"/bin/*.dll
-                ;;
-        esac
-
         # Prune libtool and object archives
         find . -name "lib*.la" -delete
         find . -name "lib*.a" -delete
