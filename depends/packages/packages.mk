9f1f281b294cdd880264f8856fcc6f30cce7d9dd
Bytzck <bytzck@protonmail.com>
2022-02-28 12:57:37 +0000

    Remove test-security-check

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,6 +1,5 @@
 packages:=boost openssl libevent zeromq gmp bls-dash backtrace cmake
 
-qrencode_packages = qrencode
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
@@ -17,8 +16,6 @@ zmq_packages=zeromq
 upnp_packages=miniupnpc
 
 darwin_native_packages = native_ds_store native_mac_alias
-
-#$(host_arch)_$(host_os)_native_packages += native_b2
 
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_libtapi native_libdmg-hfsplus
