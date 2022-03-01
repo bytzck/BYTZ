7671862916e334a30231880e754605558979fa7d
bytzck <bytzck@i2pmail.org>
2022-03-01 10:14:48 +0000

    Make windows install to

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -314,7 +314,10 @@ mkdir -p "$DISTSRC"
     # Make the os-specific installers
     case "$HOST" in
         *mingw*)
-            make deploy ${V:+V=1} BITCOIN_WIN_INSTALLER="${OUTDIR}/${DISTNAME}-win64-setup-unsigned.exe"
+            make deploy ${V:+V=1} 
+            make install DESTDIR=${OUTDIR}
+            ls ${OUTDIR}
+            sleep 20
             ;;
     esac
 
