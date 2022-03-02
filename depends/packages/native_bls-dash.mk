2a66fdb2312bce25f403c3662ace447b94b5636e
bytzck <bytzck@i2pmail.org>
2022-03-02 13:14:31 +0000

    CHange named native_bls-dash 8

--- depends/packages/native_bls-dash.mk
+++ depends/packages/native_bls-dash.mk
@@ -1,8 +1,16 @@
+package=native_bls-dash
+$(package)_version=1.1.0
+$(package)_download_path=https://github.com/dashpay/bls-signatures/archive
+$(package)_download_file=$($(package)_version).tar.gz
+$(package)_file_name=$(package)-$($(package)_download_file)
+$(package)_build_subdir=build
+$(package)_sha256_hash=276c8573104e5f18bb5b9fd3ffd49585dda5ba5f6de2de74759dda8ca5a9deac
+$(package)_dependencies=gmp cmake
+
 $(package)_version=$(bls-dash_version)
-$(package)_download_path=$(bls-dash_download_path)
+$(package)_download_path=$(blsa-dasht_download_path)
 $(package)_file_name=$(bls-dash_file_name)
 $(package)_sha256_hash=$(bls-dash_sha256_hash)
-$(package)_dependencies=gmp cmake
 
 $(package)_relic_version=3a23142be0a5510a3aa93cd6c76fc59d3fc732a5
 $(package)_relic_download_path=https://github.com/relic-toolkit/relic/archive
