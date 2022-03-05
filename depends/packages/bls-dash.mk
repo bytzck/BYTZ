1b492538a1aadee43494d953546176330dcb0f24
Bytzck <bytzck@protonmail.com>
2022-03-05 10:58:59 +0000

    Try using chia_bls file in bls-dash 2

--- depends/packages/bls-dash.mk
+++ depends/packages/bls-dash.mk
@@ -1,8 +1,8 @@
 package=bls-dash
 $(package)_version=v20181101
 $(package)_download_path=https://github.com/codablock/bls-signatures/archive
-$(package)_download_file=$($(package)_version).tar.gz
-$(package)_file_name=$(package)-$($(package)_download_file)
+#$(package)_download_file=$($(package)_version).tar.gz
+$(package)_file_name=$($(package)_version).tar.gz
 $(package)_build_subdir=build
 $(package)_sha256_hash=b3ec74a77a7b6795f84b05e051a0824ef8d9e05b04b2993f01040f35689aa87c
 $(package)_dependencies=gmp cmake
