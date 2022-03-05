b6f1eaffab9a0c77c4170e5ae5c581217e3ab83b
Bytzck <bytzck@protonmail.com>
2022-03-05 20:06:38 +0000

    Try using chia_bls file in bls-dash 13

--- depends/packages/bls-dash.mk
+++ depends/packages/bls-dash.mk
@@ -63,6 +63,7 @@ define $(package)_config_cmds
   export CFLAGS="$($(package)_cflags) $($(package)_cppflags)" && \
   export CXXFLAGS="$($(package)_cxxflags) $($(package)_cppflags)" && \
   export LDFLAGS="$($(package)_ldflags)" && \
+  echo  $($(package)_config_opts) && \
   $(host_prefix)/bin/cmake ../ $($(package)_config_opts)
 endef
 
