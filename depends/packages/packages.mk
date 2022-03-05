238c24d35ba8199bf036f07a03bab9b35c1336fc
Bytzck <bytzck@protonmail.com>
2022-03-05 10:55:17 +0000

    Try using chia_bls file in bls-dash

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,4 +1,4 @@
-packages:=boost openssl libevent zeromq gmp backtrace cmake chia_bls
+packages:=boost openssl libevent zeromq gmp backtrace cmake bls-dash
 
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
