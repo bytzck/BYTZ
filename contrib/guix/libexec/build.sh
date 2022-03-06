d0a245e8554aa504823cd16c3c1faef975abbdd5
Bytzck <bytzck@protonmail.com>
2022-03-06 10:48:06 +0000

    Final push of mac build

--- contrib/guix/libexec/build.sh
+++ contrib/guix/libexec/build.sh
@@ -303,11 +303,17 @@ mkdir -p "$DISTSRC"
     make --jobs="$JOBS" ${V:+V=1}
 
     # Check that symbol/security checks tools are sane.
-    #make test-security-check ${V:+V=1}
-    # Perform basic security checks on a series of executables.
-    make -C src --jobs=1 check-security ${V:+V=1}
-    # Check that executables only contain allowed version symbols.
-    make -C src --jobs=1 check-symbols  ${V:+V=1}
+    case "$HOST" in
+        *darwin*)
+            make -C src --jobs=1 check-symbols  ${V:+V=1}
+            ;;
+        *)
+            #make test-security-check ${V:+V=1}
+            # Perform basic security checks on a series of executables.
+            make -C src --jobs=1 check-security ${V:+V=1}
+            # Check that executables only contain allowed version symbols.
+            make -C src --jobs=1 check-symbols  ${V:+V=1}
+            ;;
 
     mkdir -p "$OUTDIR"
 
