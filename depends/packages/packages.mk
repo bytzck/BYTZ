ec438799a496255bbd41f946f68b7ea4b39c749f
Bytzck <bytzck@protonmail.com>
2022-03-06 11:47:29 +0000

    Final push of mac build 8

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -15,7 +15,7 @@ zmq_packages=zeromq
 
 upnp_packages=miniupnpc
 
-darwin_native_packages = native_ds_store native_mac_alias
+darwin_native_packages = native_biplist native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_libtapi native_libdmg-hfsplus
