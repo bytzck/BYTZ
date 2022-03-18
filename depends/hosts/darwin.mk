f57303010e4f64f1d720b9555ad6e00da28a2969
bytzck <bytzck@protonmail.com>
2022-03-18 13:10:57 +0000

    Revert back to older version of MAC SDK 10

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -87,12 +87,12 @@ $(foreach TOOL,$(cctools_TOOLS),$(eval darwin_$(TOOL) = $$(build_prefix)/bin/$$(
 #         include search paths, as that would be wrong in general but would also
 #         break #include_next's.
 #
-darwin_CC=`which env` -u C_INCLUDE_PATH -u CPLUS_INCLUDE_PATH -u OBJC_INCLUDE_PATH -u OBJCPLUS_INCLUDE_PATH -u CPATH -u LIBRARY_PATH \
+#darwin_CC=`which env` -u C_INCLUDE_PATH -u CPLUS_INCLUDE_PATH -u OBJC_INCLUDE_PATH -u OBJCPLUS_INCLUDE_PATH -u CPATH -u LIBRARY_PATH \
            clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) -B$(build_prefix)/bin --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -isysroot$(OSX_SDK)  -Xclang -internal-externc-isystem$(clang_resource_dir)/include -Xclang -internal-externc-isystem$(OSX_SDK)/usr/include
-darwin_CXX=`which env` -u C_INCLUDE_PATH -u CPLUS_INCLUDE_PATH -u OBJC_INCLUDE_PATH -u OBJCPLUS_INCLUDE_PATH -u CPATH -u LIBRARY_PATH \
+#darwin_CXX=`which env` -u C_INCLUDE_PATH -u CPLUS_INCLUDE_PATH -u OBJC_INCLUDE_PATH -u OBJCPLUS_INCLUDE_PATH -u CPATH -u LIBRARY_PATH \
            clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) -B$(build_prefix)/bin --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++  -isysroot$(OSX_SDK) -stdlib++-isystem$(OSX_SDK)/usr/include/c++/4.2.1 -Xclang -internal-externc-isystem$(OSX_SDK)/usr/include/c++/4.2.1 -Xclang -internal-externc-isystem$(clang_resource_dir)/include -Xclang -internal-externc-isystem$(OSX_SDK)/usr/include -Xclang -internal-externc-isystem$(OSX_SDK)/usr/include/c++/4.2.1/tr1 
 
-#darwin_CC=`which env` -u C_INCLUDE_PATH -u CPLUS_INCLUDE_PATH \
+darwin_CC=`which env` -u C_INCLUDE_PATH -u CPLUS_INCLUDE_PATH \
               -u OBJC_INCLUDE_PATH -u OBJCPLUS_INCLUDE_PATH -u CPATH \
               -u LIBRARY_PATH \
             $(clang_prog) --target=$(host) -mmacosx-version-min=$(OSX_MIN_VERSION) \
@@ -100,7 +100,7 @@ darwin_CXX=`which env` -u C_INCLUDE_PATH -u CPLUS_INCLUDE_PATH -u OBJC_INCLUDE_P
               -isysroot$(OSX_SDK) \
               -Xclang -internal-externc-isystem$(clang_resource_dir)/include \
               -Xclang -internal-externc-isystem$(OSX_SDK)/usr/include
-#darwin_CXX=`which env` -u C_INCLUDE_PATH -u CPLUS_INCLUDE_PATH \
+darwin_CXX=`which env` -u C_INCLUDE_PATH -u CPLUS_INCLUDE_PATH \
                -u OBJC_INCLUDE_PATH -u OBJCPLUS_INCLUDE_PATH -u CPATH \
                -u LIBRARY_PATH \
              $(clangxx_prog) --target=$(host) -mmacosx-version-min=$(OSX_MIN_VERSION) \
